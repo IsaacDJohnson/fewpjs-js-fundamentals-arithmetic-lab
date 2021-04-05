19c2bbaca977412cdc928445f20a47a39d083fef
Isaacjohnson <isaac.designws@gmail.com>
2021-04-05 14:11:31 -0500

    completed assignment

--- index.js
+++ index.js
@@ -1,2 +1,20 @@
+ let num1 = 31
 
-// Write your code here
+let num2 = 2
+
+let multiply = num1*num2
+
+console.log(num1*num2)
+
+const random = Math.floor (Math.random()* 10 + 1);
+
+console.log(random);
+
+const num3 = 18
+const num4 = 7
+const mod = num3 % num4
+
+console.log(mod)
+
+const max = Math.max(20)
+console.log(max);
